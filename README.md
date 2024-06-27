3a8da00dc09df732d0fc46995d73d28571ea6ea4
Alexander Mandrikov <mandrikov@pm.me>
2024-06-27 14:07:35 +0700

    add home-manager

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 curl -sSL https://raw.githubusercontent.com/nunsez/.dotfiles/master/install.sh | bash -
+
+# Install nix package manager
+bash <(curl -L https://nixos.org/nix/install) --no-daemon
+
+# Ensure that the necessary environment variables are set:
+. /home/alex/.nix-profile/etc/profile.d/nix.sh
+
+Add the following to ~/.config/nix/nix.conf or /etc/nix/nix.conf:
+`experimental-features = nix-command flakes`
+
+# Switch
+./switch
---
+++ home-manager/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?rev=c1a16412b44d133c7f6a37ad993bfd7323bd9180";
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager?rev=6d3b6dc9222c12b951169becdf4b0592ee9576ef";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    packages.x86_64-linux =
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      default = pkgs.home-manager;
+
+      homeConfigurations.alex = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+  };
+}
