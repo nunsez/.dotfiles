51fb0db727009ccf9b3a7d4ae457cbfc68154608
Alexander Mandrikov <mandrikov@pm.me>
2025-08-16 11:02:14 +0700

    replace stow with linker

---
+++ linker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+cd -P -- "$(dirname -- "$0")"
+
+USER_DIR="${PWD}/user"
+
+function link_user() {
+    local package=$1
+    linker relink --dir "${USER_DIR}" --target "${HOME}" "${package}"
+}
+
+link_user "fish"
+link_user "git"
+link_user "helix"
+link_user "lazygit"
+link_user "vscode"
